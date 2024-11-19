c3664339f67f5de746be497bafa766a674bcc86f
Bhargav <bhargavjangam03@gmail.com>
2024-11-19 08:53:00 +0530

    pushing some changes

---
+++ Week3-Projects/HotelRecepApplication/BookingService/app/models/request/RoomCheckoutRequest.scala
@@ -0,0 +1,11 @@
+package models.request
+
+import play.api.libs.json.{Json, Reads}
+
+
+case class RoomCheckoutRequest(roomNo: Int)
+object RoomCheckoutRequest {
+  // Define the implicit Reads for RoomCheckoutRequest
+  implicit val roomCheckoutRequestReads: Reads[RoomCheckoutRequest] = Json.reads[RoomCheckoutRequest]
+}
+
