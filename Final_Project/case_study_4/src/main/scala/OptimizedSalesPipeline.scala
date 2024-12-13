d14086f12e72172dd4ed44ff68ffc8e9400fe9e1
Bhargav <bhargavjangam03@gmail.com>
2024-12-13 19:16:00 +0530

    case_study_4 few more changes

--- Final_Project/case_study_4/src/main/scala/OptimizedSalesPipeline.scala
+++ Final_Project/case_study_4/src/main/scala/OptimizedSalesPipeline.scala
@@ -2,7 +2,8 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
-
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
+import org.apache.spark.sql.protobuf.functions.from_protobuf
 
 
 object OptimizedSalesPipeline {
@@ -20,6 +21,15 @@ object OptimizedSalesPipeline {
       .getOrCreate()
 
     import spark.implicits._
+
+    val descriptorFile = "src/main/scala/protobuf/descriptor/SalesReport.desc"
+    val messageType = "protobuf.SalesReport"
+    val store_folder_path = "gs://bhargav-assignments/final_project/case_study_4/store_data"
+    val department_folder_path = "gs://bhargav-assignments/final_project/case_study_4/department_data"
+    val metricsOutputDir = "gs://bhargav-assignments/final_project/case_study_4"
+    val enrichedParquetDir = "gs://bhargav-assignments/final_project/case_study_4/enriched_data"
+    val kafkaBootstrapServers = "localhost:9092"
+    val topic = "sales-topic"
 
     // Initial Data Loading and Dataframes
     val trainDF = spark.read.option("header", "true").option("inferSchema", "true").csv("src/main/resources/train.csv")
@@ -39,6 +49,14 @@ object OptimizedSalesPipeline {
       .join(broadCastedStoresDF, Seq("Store"), "inner")
       .join(broadCastedFeaturesDF, Seq("Store"), "inner")
 
+    println("Initial Enriched DataFrame:")
+    enrichedDF.show(5)
+    // Save enriched data
+    enrichedDF.write
+      .mode("append")
+      .partitionBy("Store", "Date")
+      .parquet(enrichedParquetDir)
+
     // Step 4: Compute Initial Store and Department Metrics
 
     // Store Metrics
@@ -50,19 +68,24 @@ object OptimizedSalesPipeline {
       )
       .cache()
 
+    println("Initial Store Metrics DataFrame:")
+    storeMetricsDF.show(5)
+
     // First Store JSON
-    storeMetricsDF.write.mode("overwrite").json("gs://bhargav-assignments/final_project/case_study_4/store_metrics.json")
+    storeMetricsDF.write.mode("overwrite").json(s"$store_folder_path/store_metrics.json")
 
     // Top 10 Stores based on total sales
     val topStoresDF = storeMetricsDF
       .orderBy(desc("Total_Sales"))
       .limit(10)
 
+    println("Initial Top Performing Stores DataFrame:")
+    topStoresDF.show(5)
     // Second Store JSON (Top 10)
-    topStoresDF.write.mode("overwrite").json("gs://bhargav-assignments/final_project/case_study_4/top_performing_stores.json")
+    topStoresDF.write.mode("overwrite").json(s"$store_folder_path/top_performing_stores.json")
 
     // Department Metrics
-    val deptMetricsDF = enrichedDF.groupBy("Store", "Dept", "Date")
+    val deptMetricsDF = enrichedDF.groupBy("Store", "Dept")
       .agg(
         sum("Weekly_Sales").alias("Total_Sales"),
         sum(when($"Is_Holiday" === true, 1).otherwise(0)).alias("Holiday_Sales"),
@@ -70,8 +93,11 @@ object OptimizedSalesPipeline {
       )
       .cache()
 
+    println("Initial Department Metrics DataFrame:")
+    deptMetricsDF.show(5)
+
     // First Department JSON (Total sales, holiday, and non-holiday sales)
-    deptMetricsDF.write.mode("overwrite").json("gs://bhargav-assignments/final_project/case_study_4/dept_metrics.json")
+    deptMetricsDF.write.mode("overwrite").json(s"$department_folder_path/dept_metrics.json")
 
     // Department trend (weekly sales, previous sales, and trend)
     val deptTrendDF = enrichedDF.groupBy("Store", "Dept", "Date")
@@ -86,15 +112,13 @@ object OptimizedSalesPipeline {
       )
       .cache()
 
+    println("Initial Department Weekly-Trends DataFrame:")
+    deptTrendDF.show(5)
+
     // Second Department JSON (Weekly sales, previous sales, and trend)
-    deptTrendDF.write.mode("overwrite").json("gs://bhargav-assignments/final_project/case_study_4/dept_trend_metrics.json")
+    deptTrendDF.write.mode("overwrite").json(s"$department_folder_path/dept_trend_metrics.json")
 
     // Step 5: Real-Time Data Processing (Streaming)
-
-    val kafkaBootstrapServers = "localhost:9092"
-    val topic = "sales-topic"
-    val enrichedParquetDir = "gs://bhargav-assignments/final_project/case_study_4/enriched_data"
-    val metricsOutputDir = "gs://bhargav-assignments/final_project/case_study_4"
 
     // Read from Kafka and process in real-time
     // Read messages from Kafka
@@ -108,8 +132,8 @@ object OptimizedSalesPipeline {
     // Extract Protobuf binary data and deserialize to DataFrame
     val salesStreamDF = kafkaDF
       .selectExpr("CAST(value AS BINARY) as value") // Extract binary Protobuf data
-      .select(from_protobuf($"value", messageType, descriptorFile).alias("salesRecord")) // Deserialize Protobuf
-      .select("salesRecord.*") // Flatten the struct for individual fields
+      .select(from_protobuf($"value", messageType, descriptorFile).alias("SalesReport")) // Deserialize Protobuf
+      .select("SalesReport.*") // Flatten the struct for individual fields
       .na.fill(Map(
         "is_holiday" -> false,      // Default boolean value
         "weekly_sales" -> 0.0f      // Default float value
@@ -123,6 +147,9 @@ object OptimizedSalesPipeline {
         val enrichedStreamDF = batchDF
           .join(broadCastedFeaturesDF, Seq("Store"), "inner")
           .join(broadCastedStoresDF, Seq("Store"), "inner")
+
+        println("New Enriched DataFrame:")
+        enrichedStreamDF.show(5)
 
         // Save enriched data
         enrichedStreamDF.write
@@ -143,21 +170,23 @@ object OptimizedSalesPipeline {
           .withColumn("Avg_Weekly_Sales", (col("Batch_Total_Sales") + col("Total_Sales")) / (col("Number_of_Sales") + col("Batch_Sales_Count")))
           .select("Store", "Total_Sales", "Number_of_Sales", "Avg_Weekly_Sales")
 
+        println("Updated Store Metrics Dataframe")
+        updatedStoreMetricsDF.show(5)
         // Save updated store metrics
-        updatedStoreMetricsDF.write.mode("overwrite").json(s"$metricsOutputDir/store_metrics.json")
-        updatedStoreMetricsDF.write.mode("overwrite").parquet(s"$metricsOutputDir/store_metrics.parquet")
+        updatedStoreMetricsDF.write.mode("overwrite").json(s"$store_folder_path/store_metrics.json")
 
         // Get Top 10 Stores by Total Sales
-        val topStoresDF = updatedStoreMetricsDF
+        val updatedTopStoresDF = updatedStoreMetricsDF
           .orderBy(desc("Total_Sales"))
           .limit(10)
 
+        println("Updated Top Performing Stores DataFrame")
+        updatedTopStoresDF.show(5)
         // Save the top-performing stores
-        topStoresDF.write.mode("overwrite").json(s"$metricsOutputDir/top_performing_stores.json")
-        topStoresDF.write.mode("overwrite").parquet(s"$metricsOutputDir/top_performing_stores.parquet")
+        updatedTopStoresDF.write.mode("overwrite").json(s"$store_folder_path/top_performing_stores.json")
 
         // Update department-level metrics
-        val updatedDeptMetricsDF = enrichedStreamDF.groupBy("Store", "Dept", "Date")
+        val updatedDeptMetricsDF = enrichedStreamDF.groupBy("Store", "Dept")
           .agg(
             sum("Weekly_Sales").alias("Weekly_Sales"),
             sum(when($"Is_Holiday" === true, 1).otherwise(0)).alias("Holiday_Sales"),
@@ -165,8 +194,10 @@ object OptimizedSalesPipeline {
           )
           .cache()
 
-        updatedDeptMetricsDF.write.mode("overwrite").json(s"$metricsOutputDir/dept_metrics.json")
-        updatedDeptMetricsDF.write.mode("overwrite").parquet(s"$metricsOutputDir/dept_metrics.parquet")
+        println("Updated Department Metrics DataFrame:")
+        updatedDeptMetricsDF.show(5)
+
+        updatedDeptMetricsDF.write.mode("overwrite").json(s"$department_folder_path/dept_metrics.json")
 
         // Department trend (weekly sales, previous sales, and trend)
         val updatedDeptTrendDF = enrichedStreamDF.groupBy("Store", "Dept", "Date")
@@ -181,8 +212,10 @@ object OptimizedSalesPipeline {
           )
           .cache()
 
+        println("Updated Department Weekly-Trends DataFrame:")
+        updatedDeptTrendDF.show(5)
         updatedDeptTrendDF.write.mode("overwrite").json(s"$metricsOutputDir/dept_trend_metrics.json")
-        updatedDeptTrendDF.write.mode("overwrite").parquet(s"$metricsOutputDir/dept_trend_metrics.parquet")
+
       }
       .outputMode("update")
       .trigger(Trigger.ProcessingTime("1 minute"))
